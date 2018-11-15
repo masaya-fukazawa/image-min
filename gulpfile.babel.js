20f1f8b2d8cd4f6dd21ab571eee1acba8d2d27b1
Masaya Fukazawa <masaya.fukazawa@bizreach.co.jp>
2018-11-15 11:46:23 +0900

    first commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp')
+const distDir = './dist';
+const srcDir = './src';
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const mozjpeg = require('imagemin-mozjpeg');
+const changed = require('gulp-changed');
+
+gulp.task('image-min', () => {
+  return gulp.src(srcDir + '/**/*.(png|jpg|gif|svg)')
+    .pipe(changed(distDir))
+    .pipe(imagemin([
+      pngquant({
+        quality: '65-80',
+        speed: 1,
+        floyd: 0,
+      }),
+      mozjpeg({
+        quality: 85,
+        progressive: true
+      }),
+      imagemin.svgo(),
+      imagemin.optipng(),
+      imagemin.gifsicle()
+    ]))
+    .pipe(gulp.dest(distDir));
+});
